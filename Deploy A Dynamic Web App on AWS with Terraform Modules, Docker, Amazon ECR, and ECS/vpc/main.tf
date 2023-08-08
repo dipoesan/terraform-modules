f805c2aea7cfff2e142d29105fc61592a0bbbe28
Oladipupo Esan <esan.dipo@gmail.com>
2023-08-08 14:32:36 +0100

    Modular VPC setup: Introduce VPC module with output file for consistent network configuration.

--- Deploy A Dynamic Web App on AWS with Terraform Modules, Docker, Amazon ECR, and ECS/vpc/main.tf
+++ Deploy A Dynamic Web App on AWS with Terraform Modules, Docker, Amazon ECR, and ECS/vpc/main.tf
@@ -29,92 +29,92 @@ resource "aws_subnet" "public_subnet_az1" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${}-${}-public-az1"
+    Name = "${var.project_name}-${var.environment}-public-az1"
   }
 }
 
 # create public subnet az2
 resource "aws_subnet" "public_subnet_az2" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_az2_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "${}-${}-public-az2"
+    Name = "${var.project_name}-${var.environment}-public-az2"
   }
 }
 
 # create route table and add public route
 resource "aws_route_table" "public_route_table" {
-  vpc_id = 
+  vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = 
-    gateway_id = 
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
   tags = {
-    Name = "${}-${}-public-rt"
+    Name = "${var.project_name}-${var.environment}-public-rt"
   }
 }
 
 # associate public subnet az1 to "public route table"
 resource "aws_route_table_association" "public_subnet_az1_rt_association" {
-  subnet_id      = 
-  route_table_id = 
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 # associate public subnet az2 to "public route table"
 resource "aws_route_table_association" "public_subnet_2_rt_association" {
-  subnet_id      = 
-  route_table_id = 
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 # create private app subnet az1
 resource "aws_subnet" "private_app_subnet_az1" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_app_subnet_az1_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "${}-${}-private-app-az1"
+    Name = "${var.project_name}-${var.environment}-private-app-az1"
   }
 }
 
 # create private app subnet az2
 resource "aws_subnet" "private_app_subnet_az2" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_app_subnet_az2_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "${}-${}-private-app-az2"
+    Name = "${var.project_name}-${var.environment}-private-app-az2"
   }
 }
 
 # create private data subnet az1
 resource "aws_subnet" "private_data_subnet_az1" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_data_subnet_az1_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "${}-${}-private-data-az1"
+    Name = "${var.project_name}-${var.environment}-private-data-az1"
   }
 }
 
 # create private data subnet az2
 resource "aws_subnet" "private_data_subnet_az2" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_data_subnet_az2_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "${}-${}-private-data-az2"
+    Name = "${var.project_name}-${var.environment}-private-data-az2"
   }
 }
