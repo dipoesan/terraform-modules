5ef8616c2d394e3938972ba4581932568fd0d2f9
Oladipupo Esan <esan.dipo@gmail.com>
2023-08-14 15:33:51 +0100

    Crafted Terraform RDS Module: Made changes to some variables

--- rds/variables.tf
+++ rds/variables.tf
@@ -1,7 +1,7 @@
 variable "project_name" {}
 variable "environment" {}
-variable "private_data_subnet_az1_cidr" {}
-variable "private_data_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
 variable "database_snapshot_identifier" {}
 variable "database_instance_class" {}
 variable "availability_zone_1" {}
