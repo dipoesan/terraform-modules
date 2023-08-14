9ece9927ff80a06ac7adaaf0d2ba9bf915f1ab98
Oladipupo Esan <esan.dipo@gmail.com>
2023-08-14 14:30:38 +0100

    Crafted Terraform RDS Module: Introduced a dedicated Terraform module for RDS setup

---
+++ rds/variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {}
+variable "environment" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
+variable "database_snapshot_identifier" {}
+variable "database_instance_class" {}
+variable "availability_zone_1" {}
+variable "database_instance_identifier" {}
+variable "multi_az_deployment" {}
+variable "database_security_group_id" {}
